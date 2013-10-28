1c95d95c7f7abab5a86badfbe457518539dc31e8
James Darling <james@abscond.org>
2013-10-28 12:02:09 +0000

    Add support for saving attorneys with an address

--- app/models/attorney.rb
+++ app/models/attorney.rb
@@ -3,4 +3,16 @@ class Attorney < ActiveResource::Base
 	schema do
 	  string  'title', 'first_name'
 	end
+
+  has_one :address
+
+  def initialize(*args)
+    # Make sure we always have associations assigned, so that activeresource doesn't request them
+    args[0] ||= {}
+    args[0] = {
+      :address => Address.new
+    }.merge(args[0])
+    super(*args)
+  end
+
 end
