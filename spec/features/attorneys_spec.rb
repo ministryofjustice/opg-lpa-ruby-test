2efb503e4425939b9443e96f921dd603ba2cc3c3
James Darling <james@abscond.org>
2013-10-30 17:27:15 +0000

    quick test for adding two attorneys

--- spec/features/attorneys_spec.rb
+++ spec/features/attorneys_spec.rb
@@ -8,12 +8,18 @@ if ENV["INTEGRATION"]
       WebMock.enable!
     end
 
-    scenario "adding one attorney" do
+    scenario "adding two attorneys" do
       create_financial_lpa_to_attorneys
       click_link 'Add an attorney'
       fill_in_valid_person(:first_name => "Attorney Juan")
       click_button "Save and continue"
-       expect(page).to have_content("Attorney Juan")
+      expect(page).to have_content("Attorney Juan")
+      click_link 'Add an attorney'
+      fill_in_valid_person(:first_name => "Attorney Tulisa")
+      click_button "Save and continue"
+      expect(page).to have_content("Attorney Juan")
+      expect(page).to have_content("Attorney Tulisa")
+
     end
 
     def fill_in_valid_person(overides={})
