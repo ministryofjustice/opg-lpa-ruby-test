302b8325c364ae03a4abc9789c0864680fb03714
James Darling <james@abscond.org>
2013-10-30 14:27:54 +0000

    Add life_sustaining step to wizard
    Only if health type. Also hides when_to_use step when property.

--- app/controllers/lpas/build_controller.rb
+++ app/controllers/lpas/build_controller.rb
@@ -2,11 +2,16 @@ class Lpas::BuildController < ApplicationController
   before_filter :check_applicant
   include Wicked::Wizard
 
-  steps :type, :donor, :when_to_use
+  steps :type, :donor, :when_to_use, :life_sustaining
 
   def show
     @lpa = Lpa.find(params[:lpa_id])
-    if step == :donor
+    case step
+    when :when_to_use
+      skip_step if @lpa.type == "Health and welfare"
+    when :life_sustaining
+      skip_step if @lpa.type == "Property and financial affairs"
+    when :donor
       @lpa.donor = @lpa.donor || Donor.new
     end
     render_wizard
