4dbb4ba040e469223c0f0e7d2dcc33a3f9565b08
James Darling <james@abscond.org>
2013-10-30 11:52:37 +0000

    Add integration test for invalid donor

--- spec/features/lpa_spec.rb
+++ spec/features/lpa_spec.rb
@@ -9,30 +9,49 @@ feature 'Filling in an LPA' do
   end
   scenario 'with all valid details' do
     visit "/"
-    fill_in 'First name', with: "Johnny"
-    fill_in 'Last name', with: "Smithson"
-    fill_in 'Post code', with: "SW1H 9AJ"
-    fill_in 'Address line1', with: "102 Petty France"
-    fill_in 'Town', with: "Westminster"
-    fill_in 'County', with: "London"
-    fill_in 'Country', with: "Great Britain"
+    fill_in_valid_person
     click_button "Save details"
 
     click_button "Create a new LPA"
 
     expect(page).to have_content('Type')
-    
+
     click_button "Save and continue"
 
     expect(page).to have_content('This LPA covers Property and financial affairs')
 
-    fill_in 'First name', with: "John"
-    fill_in 'Last name', with: "Smith"
-    fill_in 'Post code', with: "SW1H 9AJ"
-    fill_in 'Address line1', with: "102 Petty France"
-    fill_in 'Town', with: "Westminster"
-    fill_in 'County', with: "London"
-    fill_in 'Country', with: "Great Britain"
+    fill_in_valid_person
     click_button "Save and continue"
+    expect(page).to have_content("These are all the LPAs that you've created")
   end
+
+  scenario 'with all invalid donor' do
+    visit "/"
+    fill_in_valid_person
+    click_button "Save details"
+
+    click_button "Create a new LPA"
+
+    expect(page).to have_content('Type')
+
+    click_button "Save and continue"
+
+    expect(page).to have_content('This LPA covers Property and financial affairs')
+
+    fill_in_valid_person(:first_name => "")
+    click_button "Save and continue"
+    expect(page).to have_content('is too short')
+ end
+
+
+  def fill_in_valid_person(overides={})
+    fill_in 'First name', with: overides[:first_name] || "Johnny"
+    fill_in 'Last name', with: overides[:last_name] || "Smithson"
+    fill_in 'Post code', with: overides[:post_code] || "SW1H 9AJ"
+    fill_in 'Address line1', with: overides[:address_line1] || "102 Petty France"
+    fill_in 'Town', with: overides[:town] || "Westminster"
+    fill_in 'County', with: overides[:county] || "London"
+    fill_in 'Country', with: overides[:country] || "Great Britain"
+  end
+
 end
