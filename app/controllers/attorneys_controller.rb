e603e2dd2a89a04cb1502b2dd8cae39f6f9dd1f0
James Darling <james@abscond.org>
2013-10-30 21:22:15 +0000

    Removed unwritten actions in attorneys controller

--- app/controllers/attorneys_controller.rb
+++ app/controllers/attorneys_controller.rb
@@ -1,7 +1,4 @@
 class AttorneysController < ApplicationController
-  def index
-    
-  end
   def new
     @lpa = Lpa.find(params[:lpa_id])
     @attorney = Attorney.new
@@ -12,11 +9,5 @@ class AttorneysController < ApplicationController
     @lpa.attorneys << @attorney
     @lpa.save
     redirect_to lpa_build_path(:lpa_id => @lpa, :id => :attorneys)
-  end
-  def edit
-    
-  end
-  def update
-    
   end
 end
