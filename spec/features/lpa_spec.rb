ec9ab0c7022a4985f93f1b3f24933e851deaf0c8
James Darling <james@abscond.org>
2013-10-30 19:13:56 +0000

    comment each step in the integration tests to make easier to read

--- spec/features/lpa_spec.rb
+++ spec/features/lpa_spec.rb
@@ -19,20 +19,24 @@ feature 'Filling in an LPA' do
     create_financial_lpa
     expect(page).to have_content('This LPA covers Property and financial affairs')
 
+    # Donor info
     fill_in_valid_person
     click_button "Save and continue"
 
     expect(page).to have_content('The donor is Mr Johnny Smithson')
+
+    # When it can be used
     expect(page).to have_content('When to use')
     expect(page).to_not have_content('life sustaining treatment')
 
     click_button "Save and continue"
 
     expect(page).to have_content("The LPA can be used as soon as it's registered")
+  
+    # Attorneys
     expect(page).to have_content("Who are your attorneys?")
 
     click_link 'Add an attorney'
-
     fill_in_valid_person(:first_name => "Dave", :last_name => "Jameson")
 
     click_button "Save and continue"
@@ -40,6 +44,8 @@ feature 'Filling in an LPA' do
     click_link "Save and continue"
 
     expect(page).to have_content("The attorney is Mr Dave Jameson")
+
+    # Completion
     expect(page).to have_content("LPA created")
   end
 
@@ -47,9 +53,11 @@ feature 'Filling in an LPA' do
     create_healthcare_lpa
     expect(page).to have_content('This LPA covers Health and welfare')
 
+    # Donor info
     fill_in_valid_person
     click_button "Save and continue"
 
+    # Life sustaining treatment decision
     expect(page).to have_content('The donor is Mr Johnny Smithson')
     expect(page).to_not have_content('When to use')
     expect(page).to have_content('Life sustaining treatment')
