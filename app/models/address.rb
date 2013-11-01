ad4271adb9adcb982cf97c915eb6e6d03adc575e
Dom Smith <dom@teneightfive.com>
2013-11-01 11:16:23 +0000

    Added full address method

--- app/models/address.rb
+++ app/models/address.rb
@@ -9,4 +9,8 @@ class Address < ActiveResource::Base
     string  'post_code'
     string  'country'
   end
+
+  def full_address
+    "#{address_line1} #{address_line2} #{address_line3} #{town} #{county} #{post_code}"
+  end
 end
