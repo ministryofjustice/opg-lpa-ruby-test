47b53924ec417acb61d1f223761dc7904aea95e1
Rob McKinnon <@digital.justice.gov.uk>
2013-11-19 10:55:05 +0000

    to avoid InvalidAuthenticityToken during spec run

--- app/controllers/replacement_attorneys_controller.rb
+++ app/controllers/replacement_attorneys_controller.rb
@@ -1,5 +1,7 @@
 class ReplacementAttorneysController < AttorneysController
 
+  skip_before_filter :verify_authenticity_token, :only => [:destroy]
+
   def attorney_relation
     :replacement_attorneys
   end
