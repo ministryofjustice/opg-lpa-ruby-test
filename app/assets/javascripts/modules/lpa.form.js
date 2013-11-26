6225c7ce4cbee0cef4c051d8b781cb5549d59872
Dom Smith <dom@teneightfive.com>
2013-11-26 14:31:46 +0000

    Began restructuring JS to match MOJ style

---
+++ app/assets/javascripts/modules/lpa.form.js
@@ -0,0 +1,16 @@
+/*jslint browser: true, evil: false, plusplus: true, white: true, indent: 2 */
+/*global moj, lpa, $ */
+
+// Forms modules for LPA
+// Dependencies: lpa, moj, jQuery
+
+(function (){
+  "use strict";
+
+  // Add module to LPA namespace
+  lpa.Modules.form = {
+    init: function () {
+      moj.log("form init");
+    }
+  };
+}());
