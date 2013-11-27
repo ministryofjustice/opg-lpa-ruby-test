5f0116e53950810ff7511bff70971add7be70690
Dom Smith <dom@teneightfive.com>
2013-11-27 11:23:08 +0000

    Moved some scripts out into vendor folder to clean up app assets folder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,21 +10,25 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//===== Bower packages =====\\
-//= require jquery
-//= require mustache/mustache
-
-//===== MOJ object =====\\
-//= require moj
 
 //= require turbolinks
 
-//===== LPA app object and modules =====\\
+//===== Bower packages - /vendor/assets/components/ =====\\
+//= require jquery
+//= require mustache/mustache
+
+//===== Vendor scripts - /vendor/assets/javascripts/ =====\\
+//= require jquery.details
+//= require jquery.tools.min
+
+//===== MOJ boilerplate object and modules - [moj_boilerplate] Gem =====\\
+//= require moj
+
+//===== LPA app object and modules - /app/assets/javascripts/ =====\\
 //= require lpa
 //= require_tree ./modules
 
-//===== Existing app plugins and js =====\\
-//= require_tree ./jquery-plugin/jquery-details
+//===== Existing app plugins and js - /app/assets/javascripts/ =====\\
 //= require_tree ./jquery-plugin/opg
 //= require form
 //= require date-picker
