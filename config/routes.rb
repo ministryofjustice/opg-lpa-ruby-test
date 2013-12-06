b5f7f2cdf9e468d15f1bc17811ee2d081a5b461b
Hrishi Mittal <hrishimittal@gmail.com>
2013-12-06 15:12:47 +0000

    Fix PDF preview link and javascript. Clean up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ OpgLpa::Application.routes.draw do
 
   resources :lpas do
     get '/preview_pdf' => 'lpas#preview_pdf'
-    post '/preview_pdf' => 'lpas#preview_pdf'
 
     get '/get_pdf' => 'lpas#get_pdf'
 
