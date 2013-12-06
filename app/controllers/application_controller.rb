4cf56e1e0a4a2bce01bff0d7cdbdbe314ddb6a13
Jairo Diaz <jairo.diaz@codescrum.com>
2013-12-06 11:34:20 +0000

    Removed basic auth for making test similar to opg-lpa current

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASSWORD'] if Rails.env.production?
-
   def create_single_date_field model_name
     if params[model_name]
       params[model_name] = MultiparameterAttributesHandler.manipulate_all params[model_name]
