55ee328370310c14d6d2ca35f4aa19c23743e6dc
Hrishi Mittal <hrishimittal@gmail.com>
2013-12-10 17:10:26 +0000

    Make PDF integration tests pending

--- spec/features/lpa_pdf_spec.rb
+++ spec/features/lpa_pdf_spec.rb
@@ -2,32 +2,13 @@ require 'spec_helper'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 
-require 'sidekiq/testing'
-
-RSpec.configure do |config|
-  config.before(:each) do
-    # Clears out the jobs for tests using the fake testing
-    Sidekiq::Worker.clear_all
-
-    if example.metadata[:sidekiq] == :fake
-      Sidekiq::Testing.fake!
-    elsif example.metadata[:sidekiq] == :inline
-      Sidekiq::Testing.inline!
-    elsif example.metadata[:type] == :acceptance
-      Sidekiq::Testing.inline!
-    else
-      Sidekiq::Testing.fake!
-    end
-  end
-end
-
 feature 'Generating PDF previews', :js => true do
 
   before(:each) do
     sign_up_and_sign_in
   end
 
-  scenario 'for healthcare with all valid details', :vcr do
+  pending 'for healthcare with all valid details', :vcr do
     create_healthcare_lpa
     expect(page).to have_content('This LPA covers Health and welfare')
 
@@ -37,17 +18,13 @@ feature 'Generating PDF previews', :js => true do
     expect(page).to have_content('Download preview of LPA')
 
     click_link "Download preview of LPA"
-    # expect(page).to have_content('Your PDF preview is being generated. Please reload this page in a few seconds.')
-
-    sleep(10)
-    # click_link "reload this page"
-
+    sleep(3)
 
     expect(response_headers["Content-Type"]).to eq("application/pdf")
     expect(response_headers["Content-Disposition"]).to eq("inline; filename=\"draft.pdf\"")
   end
 
-  scenario 'for financial with all valid details', :vcr do
+  pending 'for financial with all valid details', :vcr do
     create_financial_lpa
     expect(page).to have_content('This LPA covers Property and financial affairs')
 
@@ -57,7 +34,7 @@ feature 'Generating PDF previews', :js => true do
     expect(page).to have_content('Download preview of LPA')
 
     click_link "Download preview of LPA"
-    sleep(10)
+    sleep(3)
 
     expect(response_headers["Content-Type"]).to eq("application/pdf")
     expect(response_headers["Content-Disposition"]).to eq("inline; filename=\"draft.pdf\"")
