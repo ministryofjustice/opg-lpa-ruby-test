2532ada48fb28a03f7bab65a14b7c485d9fdd922
Hrishi Mittal <hrishimittal@gmail.com>
2013-12-11 14:28:42 +0000

    Fix integration tests for PDF previews

--- spec/features/lpa_pdf_spec.rb
+++ spec/features/lpa_pdf_spec.rb
@@ -6,9 +6,10 @@ feature 'Generating PDF previews', :js => true do
 
   before(:each) do
     sign_up_and_sign_in
+    system("rm -f pdfs/drafts/*.pdf")
   end
 
-  pending 'for healthcare with all valid details', :vcr do
+  scenario 'for healthcare with all valid details', :vcr do
     create_healthcare_lpa
     expect(page).to have_content('This LPA covers Health and welfare')
 
@@ -24,7 +25,7 @@ feature 'Generating PDF previews', :js => true do
     expect(response_headers["Content-Disposition"]).to eq("inline; filename=\"draft.pdf\"")
   end
 
-  pending 'for financial with all valid details', :vcr do
+  scenario 'for financial with all valid details', :vcr do
     create_financial_lpa
     expect(page).to have_content('This LPA covers Property and financial affairs')
 
