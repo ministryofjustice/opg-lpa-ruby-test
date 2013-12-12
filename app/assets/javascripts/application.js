0ba4150189bd9dd45f3ba3f42faf9c6fb2f562c4
Dom Smith <dom@teneightfive.com>
2013-12-12 16:42:38 +0000

    Add Js validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,8 @@
 //===== Bower packages - /vendor/assets/components/ =====\\
 //= require jquery
 //= require jquery-ujs
+//= require pubsub-js
+//= require parsleyjs/parsley
 
 //===== Vendor scripts - /vendor/assets/javascripts/ =====\\
 //= require jquery.details
@@ -11,17 +13,18 @@
 //===== MOJ boilerplate object and modules - [moj_boilerplate] Gem =====\\
 //= require moj
 
+//===== Existing app plugins and js - /app/assets/javascripts/ =====\\
+//= require_tree ./jquery-plugin/opg
+//= require form
+//= require date-picker
+//= require pwstrength
+
 //===== LPA app object and modules - /app/assets/javascripts/ =====\\
 //= require utils
 //= require lpa
 //= require modules/lpa.dialog.js
 //= require modules/lpa.popup.js
 //= require modules/lpa.help-system.js
+//= require modules/lpa.form-popup.js
 //= require modules/lpa.form.js
 //= require modules/lpa.poller.js
-
-//===== Existing app plugins and js - /app/assets/javascripts/ =====\\
-//= require_tree ./jquery-plugin/opg
-//= require form
-//= require date-picker
-//= require pwstrength
