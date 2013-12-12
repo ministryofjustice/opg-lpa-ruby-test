41eb91c2a8701b5db01268e42ae00fba0dad29d1
Dom Smith <dom@teneightfive.com>
2013-12-12 16:46:57 +0000

    Add defaults for ParsleyJS form validation

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -6,3 +6,33 @@ function html5_storage() {
     return false;
   }
 }
+
+// Update parsley defaults
+window.ParsleyConfig = window.ParsleyConfig || {};
+
+(function ($) {
+  window.ParsleyConfig = $.extend( true, {}, window.ParsleyConfig, {
+    messages: {
+      defaultMessage: "in invalid",
+      type: {
+        email:      "must be a valid email address"
+      },
+      required:       "can't be blank",
+      min:            "is too short (minimum is %s characters)",
+      max:            "is too long (maximum is %s characters)"
+    },
+    trigger: 'change',
+    animate: false,
+    errorClass: 'validation',
+    errors: {
+      errorElem: '<span class="validation-message"></span>',
+      errorsWrapper: '<span></span>',
+      classHandler: function (elem, isRadioOrCheckbox) {
+        return $(elem).parent();
+      },
+      container: function (elem, isRadioOrCheckbox) {
+        return elem.parent().find("label");
+      }
+    }
+  });
+}(window.jQuery));
