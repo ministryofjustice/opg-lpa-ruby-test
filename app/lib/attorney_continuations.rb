e7282f56c76da4cf2c99231198faaf7ff5496d63
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-12-13 17:20:31 +0000

    Form filling for more than two attorneys
    The correct hash is being returned.
    Whose result needs to be applied to PDF document,
    which then needs to be concatenated to the main
    LPA pdf.

---
+++ app/lib/attorney_continuations.rb
@@ -0,0 +1,42 @@
+class AttorneyContinuations
+
+  def initialize(total_sheets, sheet_no, data)
+    @data = data
+    @hash = {}
+    @hash["totalSheets"] = total_sheets.to_s
+    @hash["sheetNumber"] = sheet_no.to_s
+  end
+
+  def to_form_data
+    @hash["Line1"] = first_line
+    @hash["Line2"] = get_full_name
+    @hash["Line3"] = get_address
+    @hash["Line4"] = ""
+    @hash["Line5"] = get_dob
+    @hash
+  end
+
+  private
+
+  def first_line
+    num = (@hash["sheetNumber"].to_i + 2).to_words ordinal: true, remove_hyphen: true
+    "#{num.capitalize} attorney"
+  end
+
+  def get_full_name
+    "#{@data["title"]} #{@data["first_name"]} #{@data["last_name"]}"
+  end
+
+  def get_address
+    arr = []
+    ["address_line1", "address_line2", "address_line3",
+     "town", "county", "post_code", "country"].each do |entry|
+      arr << @data["address"][entry]
+    end
+    arr.join(', ')
+  end
+
+  def get_dob
+    "DOB: #{@data["date_of_birth"]}"
+  end
+end
