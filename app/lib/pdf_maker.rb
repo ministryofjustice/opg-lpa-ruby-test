d7ed37e2cee5c9660c5f8cf9340e3c66ee3c0a7b
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-12-17 10:22:59 +0000

    Using XFDFMaker

--- app/lib/pdf_maker.rb
+++ app/lib/pdf_maker.rb
@@ -9,7 +9,7 @@ class PDFMaker
   end
 
   def to_pdf
-    xfdf = create_xfdf(create_donor)
+    xfdf = XFDFMaker.new(create_donor).create
     populate_form(xfdf)
   end
 
@@ -26,24 +26,5 @@ class PDFMaker
 
   def create_donor
     JSONFormatter.new(@lpa_json).to_form_data
-  end
-
-  def create_xfdf(fields)
-    xml = Builder::XmlMarkup.new
-    xml.instruct!
-    xml.xfdf("xmlns" => "http://ns.adobe.com/xfdf/", "xml:space" => "preserve") {
-      xml.fields {
-        fields.each do |field, value|
-          xml.field(:name => field) {
-            if value.is_a? Array
-              value.each {|item| xml.value(item.to_s) }
-            else
-              xml.value(value.to_s)
-            end
-          }
-        end
-      }
-    }
-    xml.target!
   end
 end
