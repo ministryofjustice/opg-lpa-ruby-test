a75cfdc4f7d45032f1f4e52030660d46a1012aa0
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-12-18 12:10:15 +0000

    The continuation is being appended
    Also moved & really added the files under support

--- spec/lib/pdf_maker_spec.rb
+++ spec/lib/pdf_maker_spec.rb
@@ -5,7 +5,7 @@ describe PDFMaker do
     before(:each) do
       system("rm -f #{@pdf_filename}")
       lpa_id = "529609ed77696e052c490100"
-      @doc = PDFMaker.new(lpa_id, donor_json).to_pdf
+      PDFMaker.new(lpa_id, donor_json).to_pdf
       @pdf_filename = "#{Rails.root}/pdfs/drafts/#{lpa_id}.pdf"
     end
 
@@ -17,5 +17,20 @@ describe PDFMaker do
       good_md5 = Digest::MD5.hexdigest(File.read("spec/support/sample_lpa.pdf"))
       Digest::MD5.hexdigest(File.read(@pdf_filename)).should eq good_md5
     end
+
+    context "when there are more than 2 attorneys" do
+      describe "the generated PDF" do
+
+        it "should have a continuation sheet attached" do
+          lpa_id = "52a72c0377696e03df020000"
+          pdf = PDFMaker.new(lpa_id, three_attorneys_json).to_pdf
+          pages = %x[pdftk #{pdf} dump_data | awk '/NumberOfPages/ {print $2}']
+          pages.to_i.should eq 16
+        end
+
+        it "should have attorney data"
+      end
+    end
+
   end
 end
