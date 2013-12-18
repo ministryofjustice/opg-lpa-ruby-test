c9fe56f4d334afbf35212b6af0814eb4219f2933
Dom Smith <dom@teneightfive.com>
2013-12-18 13:18:30 +0000

    Improve form popup and use handlebars templates for validation

---
+++ app/assets/javascripts/lpa.templates.js
@@ -0,0 +1,48 @@
+this["lpa"] = this["lpa"] || {};
+this["lpa"]["templates"] = this["lpa"]["templates"] || {};
+
+this["lpa"]["templates"]["validation-summary"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n  <p>Because of the following problems:</p>\n  <ol>\n    ";
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.errors), {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n  </ol>\n  ";
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n    <li>\n      <a href=\"#";
+  if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.name); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "_label\">";
+  if (stack1 = helpers.label) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.label); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + " ";
+  if (stack1 = helpers.error) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
+  else { stack1 = (depth0 && depth0.error); stack1 = typeof stack1 === functionType ? stack1.call(depth0, {hash:{},data:data}) : stack1; }
+  buffer += escapeExpression(stack1)
+    + "</a>\n    </li>\n    ";
+  return buffer;
+  }
+
+function program4(depth0,data) {
+  
+  
+  return "\n  <p>Please reload the page and try again.</p>\n  ";
+  }
+
+  buffer += "<div aria-labbelledby=\"error-heading\" class=\"validation-summary group\" role=\"alert\">\n  <h1 id=\"error-heading\">There was a problem submitting the form</h1>\n\n  ";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.errors), {hash:{},inverse:self.program(4, program4, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n</div>";
+  return buffer;
+  });
