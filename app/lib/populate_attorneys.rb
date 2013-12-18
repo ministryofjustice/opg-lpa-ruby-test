62ef0e30ee756e426ea08bc41bda901013df491b
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2013-12-18 15:39:19 +0000

    Refactored the way continuations are added

--- app/lib/populate_attorneys.rb
+++ app/lib/populate_attorneys.rb
@@ -11,10 +11,8 @@ class PopulateAttorneys
     count = 0
     attorneys.each do |attorney|
       count += 1
-      continuation_data = AttorneyContinuations.new(total_continuations, count, attorney)
-      populate_A1 continuation_data
-      populate_C1 continuation_data
-      concatenate_continuation
+      populate_continuations AttorneyContinuations.new(total_continuations, count, attorney)
+      concatenate_continuations
     end
   end
 
@@ -24,25 +22,32 @@ class PopulateAttorneys
     @data["attorneys"].drop(2)
   end
 
+  def populate_continuations data
+    populate_A1 data
+    populate_C1 data
+  end
+
   def populate_A1(data)
-    xfdf = XFDFMaker.new(data.continuationA1).create
     path = File.open("templates/continuation_A1_#{@lpa_id}.xfdf", 'w+').path
-    File.open(path, 'w') {|f| f.write(xfdf) }
     template =  File.join(Rails.root, "templates", "PFA_continuation_A1.pdf")
     result = File.join("pdfs", "drafts", "continuation_A1_#{@lpa_id}.pdf")
-    system "pdftk #{template} fill_form #{path} output #{result} flatten"
+    fill_pdf data.continuationA1, path, template, result
   end
 
   def populate_C1(data)
-    xfdf = XFDFMaker.new(data.continuationC1).create
     path = File.open("templates/continuation_C_#{@lpa_id}.xfdf", 'w+').path
-    File.open(path, 'w') {|f| f.write(xfdf) }
     template =  File.join(Rails.root, "templates", "PFA_continuation_C.pdf")
     result = File.join("pdfs", "drafts", "continuation_C_#{@lpa_id}.pdf")
+    fill_pdf data.continuationC1, path, template, result
+  end
+
+  def fill_pdf(data, path, template, result)
+    xfdf = XFDFMaker.new(data)
+    File.open(path, 'w') {|f| f.write(xfdf.create) }
     system "pdftk #{template} fill_form #{path} output #{result} flatten"
   end
 
-  def concatenate_continuation
+  def concatenate_continuations
     lpa = File.join("pdfs", "drafts", "#{@lpa_id}.pdf")
     a1 = File.join("pdfs", "drafts", "continuation_A1_#{@lpa_id}.pdf")
     c =  File.join("pdfs", "drafts", "continuation_C_#{@lpa_id}.pdf")
