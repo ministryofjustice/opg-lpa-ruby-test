a419ba2007833fbceb128b35fcfe047dd1d56216
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-01-09 10:55:09 +0000

    Implemented form filling for section 5
    For option:
    
    'Jointly for some decisions,
    and jointly and severally for other decisions'
    
    filling in the form below

--- spec/lib/json_formatter_spec.rb
+++ spec/lib/json_formatter_spec.rb
@@ -173,6 +173,171 @@ def three_attorneys_hash
   }
 end
 
+def attorney_decisions_jointly_for_some
+  '{
+    "donor": {
+        "address": {
+            "address_line1": "3 Streety Street",
+            "address_line2": "",
+            "address_line3": "",
+            "town": "London",
+            "county": "",
+            "post_code": "SW1",
+            "country": "United Kingdom"
+        },
+        "date_of_birth": "1900-01-01",
+        "title": "Mr",
+        "first_name": "FirstName",
+        "middle_names": "",
+        "last_name": "LastName",
+        "phone": "",
+        "email": ""
+    },
+    "certificate_provider": null,
+    "certificate_provider2": null,
+    "people_to_be_told": [],
+    "attorneys": [
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe33977696e6f0a000000",
+            "date_of_birth": "1894-01-01",
+            "title": "Mr",
+            "first_name": "First",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe34a77696e6f0a040000",
+            "date_of_birth": "1894-01-01",
+            "title": "Mr",
+            "first_name": "Second",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe3a577696e6f0a0a0000",
+            "date_of_birth": "1925-02-01",
+            "title": "Ms",
+            "first_name": "Third",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cc0aa577696e724c120000",
+            "date_of_birth": "1919-02-01",
+            "title": "Ms",
+            "first_name": "Yes",
+            "middle_names": "",
+            "last_name": "No",
+            "phone": "",
+            "email": ""
+        }
+    ],
+    "replacement_attorneys": [],
+    "id": "52cae0c677696e39bc260000",
+    "uri": "http://localhost/api/lpas.json/52cae0c677696e39bc260000.json",
+    "type": "Property and financial affairs",
+    "when_to_use": "only if I don\'t have mental capacity",
+    "how_attorneys_act": "Jointly for some decisions, and jointly and severally for other decisions",
+    "how_attorneys_act_details": "This is grand",
+    "applicant": {
+        "address": {
+            "address_line1": "3 Streety Street",
+            "address_line2": "",
+            "address_line3": "",
+            "town": "London",
+            "county": "",
+            "post_code": "SW1",
+            "country": "United Kingdom"
+        },
+        "lpas": [],
+        "id": "52cae0c477696e39bc240000",
+        "uri": "http://localhost/api/applicants.json/52cae0c477696e39bc240000.json",
+        "date_of_birth": "1900-01-01",
+        "title": "Mr",
+        "first_name": "FirstName",
+        "middle_names": "",
+        "last_name": "LastName"
+    }
+  }'
+end
+
+def jointly_for_some_hash
+  {
+    "Page1A1SheetCount" => 2,
+    "Page1CSheetCount" => 2,
+    "Page1TotalContSheetCount" => 4,
+    "PartAAttorney1Address1" => "",
+    "PartAAttorney1Address2" => "",
+    "PartAAttorney1Address3" => "",
+    "PartAAttorney1DOB" => "1894-01-01",
+    "PartAAttorney1FirstName" => "First",
+    "PartAAttorney1LastName" => "Attorney",
+    "PartAAttorney1Postcode" => "",
+    "PartAAttorney2Address1" => "",
+    "PartAAttorney2Address2" => "",
+    "PartAAttorney2Address3" => "",
+    "PartAAttorney2DOB" => "1894-01-01",
+    "PartAAttorney2FirstName" => "Second",
+    "PartAAttorney2LastName" => "Attorney",
+    "PartAAttorney2Postcode" => "",
+    "PartADonorAddress1" => "3 Streety Street",
+    "PartADonorAddress2" => "",
+    "PartADonorAddress3" => "",
+    "PartADonorDOB" => "1900-01-01",
+    "cb_Page3DonorTitle" => "Mr",
+    "PartADonorFirstName" => "FirstName",
+    "PartADonorLastName" => "LastName",
+    "PartADonorOtherNames" => "",
+    "PartADonorPostcode" => "SW1",
+    "Restrictions" => "I would like my LPA to take effect only when I lose capacity to make decisions about my property and financial affairs",
+    "AttorneyDecisionsInstructions" => "This is grand",
+    "Restrictions" => "I would like my LPA to take effect only when I lose capacity to make decisions about my property and financial affairs",
+  }
+end
+
+
 describe JSONFormatter do
 
   context "when only a donor is present" do
@@ -204,6 +369,14 @@ describe JSONFormatter do
 
     it "should create the correct field" do
       formatter.to_form_data.keys.should include "Restrictions"
+    end
+  end
+
+  context "when the section 5, for property is filled in" do
+    let(:formatter) { JSONFormatter.new(attorney_decisions_jointly_for_some) }
+
+    it "should create the correct field" do
+      formatter.to_form_data.should eq jointly_for_some_hash
     end
   end
 
