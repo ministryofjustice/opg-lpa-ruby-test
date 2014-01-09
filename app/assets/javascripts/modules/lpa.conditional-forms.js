c703b234cefd23cc23296aa836f53d1189f7fc7c
Dom Smith <dom@teneightfive.com>
2014-01-09 15:19:59 +0000

    Strip back to minimum functionality for now

--- app/assets/javascripts/modules/lpa.conditional-forms.js
+++ app/assets/javascripts/modules/lpa.conditional-forms.js
@@ -7,32 +7,9 @@
   moj.Modules.ConditionalForms = {
     init: function () {
       moj.log('lpa.modules.ConditionalForms#init');
-      _.bindAll(this, 'donorSign');
-      this.cacheEls();
-      this.bindEvents();
 
       // init conditional content plugins
       $('.js-conditional-content').conditionalContent();
-    },
-
-    cacheEls: function () {
-      this.$body = $('body');
-    },
-
-    bindEvents: function () {
-      this.$body.on('change', '#donor_cannot_sign', this.donorSign);
-    },
-
-    donorSign: function (e) {
-      this.toggleEl($(e.target), $(e.target).is(':checked'));
-    },
-
-    toggleEl: function (el, bool) {
-      if (bool) {
-        el.show();
-      } else {
-        el.hide();
-      }
     }
   };
 
