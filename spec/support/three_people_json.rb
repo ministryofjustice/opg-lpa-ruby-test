5d59822922c176ce1154be2cd07eef7b610e796e
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-01-09 17:34:44 +0000

    Moved PDF config files under pdf_config dir
    In order to keep them separate from the
    Rails config files
    
    For some reason, 3rd person to be told is not being
    processed. Will resolve this!

---
+++ spec/support/three_people_json.rb
@@ -0,0 +1,210 @@
+def three_people_json
+  '{
+    "donor": {
+        "address": {
+            "address_line1": "3 Streety Street",
+            "address_line2": "",
+            "address_line3": "",
+            "town": "London",
+            "county": "",
+            "post_code": "SW1",
+            "country": "United Kingdom"
+        },
+        "date_of_birth": "1900-01-01",
+        "title": "Mr",
+        "first_name": "FirstName",
+        "middle_names": "",
+        "last_name": "SecondName",
+        "phone": "",
+        "email": ""
+    },
+    "certificate_provider": {
+        "address": {
+            "address_line1": "",
+            "address_line2": "",
+            "address_line3": "",
+            "town": "",
+            "county": "",
+            "post_code": "",
+            "country": ""
+        },
+        "title": "Mr",
+        "first_name": "First",
+        "middle_names": "",
+        "last_name": "Last",
+        "email": ""
+    },
+    "certificate_provider2": null,
+    "people_to_be_told": [
+        {
+            "address": {
+                "address_line1": "1 PTBT Street",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "London",
+                "county": "",
+                "post_code": "W1",
+                "country": "London"
+            },
+            "title": "Mr",
+            "first_name": "PersonToBeTold",
+            "middle_names": "",
+            "last_name": "PersonToBeToldLastname"
+        },
+        {
+            "address": {
+                "address_line1": "2 Secondy Street",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "London",
+                "county": "",
+                "post_code": "W2",
+                "country": ""
+            },
+            "title": "Miss",
+            "first_name": "Lady2",
+            "middle_names": "",
+            "last_name": "LadyLady2"
+        },
+        {
+            "address": {
+                "address_line1": "1 Buckingham Palace Road",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "London",
+                "county": "",
+                "post_code": "SW1A 1AA",
+                "country": "United Kingdom"
+            },
+            "title": "Dr",
+            "first_name": "ThirdPerson",
+            "middle_names": "",
+            "last_name": "ThirdPersonLastName"
+        }
+    ],
+    "attorneys": [
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe33977696e6f0a000000",
+            "date_of_birth": "1894-01-01",
+            "title": "Mr",
+            "first_name": "First",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe34a77696e6f0a040000",
+            "date_of_birth": "1894-01-01",
+            "title": "Mr",
+            "first_name": "Second",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cbe3a577696e6f0a0a0000",
+            "date_of_birth": "1925-02-01",
+            "title": "Ms",
+            "first_name": "Third",
+            "middle_names": "",
+            "last_name": "Attorney",
+            "phone": "",
+            "email": ""
+        },
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52cc0aa577696e724c120000",
+            "date_of_birth": "1919-02-01",
+            "title": "Ms",
+            "first_name": "Yes",
+            "middle_names": "",
+            "last_name": "No",
+            "phone": "",
+            "email": ""
+        }
+    ],
+    "replacement_attorneys": [
+        {
+            "address": {
+                "address_line1": "",
+                "address_line2": "",
+                "address_line3": "",
+                "town": "",
+                "county": "",
+                "post_code": "",
+                "country": ""
+            },
+            "id": "52ce8f7077696e724cc00000",
+            "date_of_birth": "1894-01-01",
+            "title": "Mr",
+            "first_name": "Replacement Attorney",
+            "middle_names": "",
+            "last_name": "RA LastName",
+            "phone": "",
+            "email": ""
+        }
+    ],
+    "id": "52cae0c677696e39bc260000",
+    "uri": "http://localhost/api/lpas.json/52cae0c677696e39bc260000.json",
+    "type": "Property and financial affairs",
+    "when_to_use": "only if I don\'t have mental capacity",
+    "how_attorneys_act": "Jointly for some decisions, and jointly and severally for other decisions",
+    "how_attorneys_act_details": "This is grand",
+    "applicant": {
+        "address": {
+            "address_line1": "3 Streety Street",
+            "address_line2": "",
+            "address_line3": "",
+            "town": "London",
+            "county": "",
+            "post_code": "SW1",
+            "country": "United Kingdom"
+        },
+        "lpas": [],
+        "id": "52cae0c477696e39bc240000",
+        "uri": "http://localhost/api/applicants.json/52cae0c477696e39bc240000.json",
+        "date_of_birth": "1900-01-01",
+        "title": "Mr",
+        "first_name": "FirstName",
+        "middle_names": "",
+        "last_name": "SecondName"
+    }
+  }'
+end
