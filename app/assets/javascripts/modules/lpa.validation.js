d07c95e06a0a971ccb33c3348482c3945819f483
Dom Smith <dom@teneightfive.com>
2014-01-10 11:52:41 +0000

    Move parsley config into Validation module file

--- app/assets/javascripts/modules/lpa.validation.js
+++ app/assets/javascripts/modules/lpa.validation.js
@@ -1,3 +1,36 @@
+// Update parsley defaults
+window.ParsleyConfig = window.ParsleyConfig || {};
+(function () {
+  'use strict';
+
+  window.ParsleyConfig = $.extend(true, {}, window.ParsleyConfig, {
+    messages: {
+      defaultMessage: 'in invalid',
+      type: {
+        email:     'must be a valid email address'
+      },
+      required:    'can\'t be blank',
+      minlength:   'is too short (minimum is %s characters)',
+      maxlength:   'is too long (maximum is %s characters)'
+    },
+    trigger: 'change',
+    scrollDuration: 0,
+    animate: false,
+    errorClass: 'validation',
+    errors: {
+      errorElem: '<span class="validation-message"></span>',
+      errorsWrapper: '<span></span>',
+      classHandler: function (elem) {
+        return $(elem).parent();
+      },
+      container: function (elem) {
+        return elem.parent().find('label');
+      }
+    }
+  });
+}());
+
+// Validation module
 (function () {
   'use strict';
 
