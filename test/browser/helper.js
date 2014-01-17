1c0d2d450c58b3d932f83af7efbeb08c464ad814
Dom Smith <dom@teneightfive.com>
2014-01-17 17:10:24 +0000

    Add initial tests for postcode lookup module

--- test/browser/helper.js
+++ test/browser/helper.js
@@ -1,3 +1,52 @@
-module.exports = {
-  url: 'http://0.0.0.0:3000/'
+var helper = {
+  url: 'http://0.0.0.0:3000/',
+  username: 'john.stevens@digital.co.uk',
+  password: 'tardis123',
+
+  createAccount: function (test) {
+    test
+      .open(helper.url + 'users/sign_up')
+      .type('[name="registration[email]"]', helper.username)
+      .type('[name="registration[password]"]', helper.password)
+      .click('[type="submit"]')
+      .done();
+  },
+
+  login: function (test) {
+    'use strict';
+
+    test
+      .open(helper.url + 'users/sign_in')
+      .type('[name="session[email]"]', helper.username)
+      .type('[name="session[password]"]', helper.password)
+      .click('[type="submit"]')
+      .done();
+  },
+
+  fillDetails: function (test) {
+    'use strict';
+
+    test
+      .open(helper.url + 'applicants/new')
+      .type('[name="applicant[first_name]"]', 'John')
+      .type('[name="applicant[last_name]"]', 'Smith')
+      .type('[name="applicant[date_of_birth(3i)]"]', '10')
+      .type('[name="applicant[date_of_birth(2i)]"]', 'January')
+      .type('[name="applicant[date_of_birth(1i)]"]', '1980')
+      .click('input[value="Save and continue"]')
+      .done();
+  },
+
+  killStorage: function (test) {
+    'use strict';
+
+    test
+      .execute(function () {
+        window.sessionstorage.clear();
+        window.localstorage.clear();
+      })
+      .wait(500)
+      .done();
+  }
 };
+module.exports = helper;
---
+++ test/browser/postcode_lookup_test.js
@@ -0,0 +1,72 @@
+var helper = require('./helper');
+
+module.exports = {
+  name: 'Postcode Lookup',
+
+  'create account': helper.createAccount,
+  'login to account': helper.login,
+  // 'fill in details': helper.fillDetails,
+
+  'create LPA': function (test) {
+    'use strict';
+
+    test
+      .open(helper.url + 'lpas')
+      .click('input[value="Create a new LPA"]', 'click create LPA button')
+      .click('label[for="lpa_type_property_and_financial_affairs"]', 'select property and financial LPA')
+      .click('input[value="Save and continue"]', 'continue to step 2')
+      .done();
+  },
+
+  'empty postcode value should return error': function (test) {
+    test
+      .click('.js-PostcodeLookup__search-btn', 'click postcode search button')
+      // temp disable until bug with phantomjs is fixed
+      // .assert.dialogText('Please enter a postcode', 'empty postcode validation message appears')
+      .accept()
+      .done();
+  },
+
+  'postcode search value should return results in select': function (test) {
+    test
+      .type('.js-PostcodeLookup__query', 'SW1', 'enter value into postcode search query')
+      .click('.js-PostcodeLookup__search-btn', 'click postcode search button')
+      .waitForElement('.js-PostcodeLookup__search-results')
+      .assert.visible('select.js-PostcodeLookup__search-results', 'search results have been display in select field')
+      .end()
+      .done();
+  },
+
+  'changing option in select should populate address': function (test) {
+    test
+      .type('.js-PostcodeLookup__search-results', 'Flat', 'select address from results')
+      .click('[for="donor_cannot_sign"]', 'click different element to trigger change')
+      .assert.chain()
+        .visible('.js-PostcodeLookup__postal-add', 'address fieldset is visible')
+        .doesntExist('.js-PostcodeLookup__search-results', 'search results have been removed')
+      .end()
+      .done();
+  },
+
+  'selecting manual address shows address fields': function (test) {
+    test
+      .reload()
+      .click('.js-PostcodeLookup__toggle-address[data-address-type="postal"]', 'click manual address toggle')
+      .assert.chain()
+        .visible('.js-PostcodeLookup__postal-add', 'address fieldset is visible')
+        .notVisible('.js-PostcodeLookup__dx-add', 'dx fieldset is not visible')
+      .end()
+      .done();
+  },
+
+  'selecting dx address shows dx fields': function (test) {
+    test
+      .reload()
+      .click('.js-PostcodeLookup__toggle-address[data-address-type="dx"]', 'click manual address toggle')
+      .assert.chain()
+        .visible('.js-PostcodeLookup__dx-add', 'dx fieldset is visible')
+        .notVisible('.js-PostcodeLookup__postal-add', 'address fieldset is not visible')
+      .end()
+      .done();
+  }
+};
